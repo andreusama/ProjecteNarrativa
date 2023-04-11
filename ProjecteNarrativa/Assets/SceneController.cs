053dfedcce9fae5ac984c8cd591d581c3fb8f43f
andres <50046952+andreusama@users.noreply.github.com>
2023-04-11 23:16:30 +0200

    Scaping of the prison FX and prefab iteration

---
+++ ProjecteNarrativa/Assets/SceneController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    //create two transforms with name of spawn points
+    public Transform spawnPointMentor;
+    public Transform spawnPointScaping;
+
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
